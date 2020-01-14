aad93aabe5a2376a5360ba0ae1c5700cb118a18d
Robert Collins <rpc219@gmail.com>
2020-01-14 16:40:03 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,14 +1,16 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
+  status = "It's-a me, Mario!"
+  puts status
 end
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
 
 def link
-
+  status = "It's dangerous to go alone! Take this."
+  puts status
 end
 
 def any_phrase
